83b214934885399652ceedc29e57e4c582dc0d90
Scott Powers <saberpowers@gmail.com>
2024-07-27 10:40:46 -0500

    put run values on approach changes

--- scripts/download_data.R
+++ scripts/download_data.R
@@ -3,20 +3,19 @@
 
 cluster <- parallel::makeCluster(parallel::detectCores())
 data_baseballsavant <- sabRmetrics::download_baseballsavant(
-  start_date = "2024-01-01",
+  start_date = "2024-04-03",  # first day of swing tracking data
   end_date = "2024-12-31",
   cl = cluster
 )
 parallel::stopCluster(cluster)
 
 data_baseballsavant |>
-  dplyr::filter(!is.na(bat_speed), !is.na(swing_length)) |>
   dplyr::select(
-    game_id, event_index, pitch_number, batter_id, bat_side, batter_name,
+    game_date, game_id, event_index, pitch_number, batter_id, bat_side, batter_name,
     inning, half_inning, outs, balls, strikes,
     ax, ay, az, vx0, vy0, vz0, release_pos_x, release_pos_y, release_pos_z,
     pitch_type, release_speed, plate_x, plate_z, strike_zone_top, strike_zone_bottom,
     bat_speed, swing_length, launch_speed, launch_angle, hit_coord_x, hit_coord_y,
     description, des
   ) |>
-  data.table::fwrite(file = "data/swing.csv")
+  data.table::fwrite(file = "data/baseballsavant.csv")
--- scripts/estimate_models.R
+++ scripts/estimate_models.R
@@ -3,15 +3,24 @@ devtools::load_all("package/swingfastslow")
 
 # Load data and upstream models ----
 
-swing <- data.table::fread("data/swing.csv")
+data <- data.table::fread("data/baseballsavant.csv")
+linear_weight <- data.table::fread("models/linear_weight.csv")
 pitch_outcome_model <- readRDS("models/pitch_outcome_model.rds")
 hit_outcome_model <- readRDS("models/hit_outcome_model.rds")
 
-# Estimate intention model ----
-
-data_intention <- swing |>
+data_with_pred <- data |>
   sabRmetrics::get_quadratic_coef(source = "baseballsavant") |>
   sabRmetrics::get_trackman_metrics() |>
+  predict_pitch_hit_outcomes(
+    pitch_outcome_model = pitch_outcome_model,
+    hit_outcome_model = hit_outcome_model
+  )
+
+
+# Estimate intention model ----
+
+data_intention <- data_with_pred |>
+  dplyr::filter(!is.na(bat_speed), !is.na(swing_length)) |>
   remove_partial_swings() |>
   recreate_squared_up() |>
   dplyr::mutate(
@@ -23,10 +32,52 @@ data_intention <- swing |>
 
 intention_model <- fit_intention_model(data_intention)
 
+
 # Estimate approach model ----
 
 data_approach <- data_intention |>
-  predict_pitch_hit_outcomes(pitch_outcome_model = pitch_outcome_model, hit_outcome_model = hit_outcome_model) |>
   dplyr::inner_join(intention_model$approach, by = "batter_side_id")
 
 approach_model <- fit_approach_model(data_approach)
+
+
+# Caculate the run value of different approaches ----
+
+approach_grid <- intention_model$approach |>
+  with(
+    data.frame(
+      strikes_bat_speed = c(-3:3, -1, 1) * sd(strikes_bat_speed),
+      strikes_swing_length = c(-3:3, 1, -1) * sd(strikes_swing_length)
+    )
+  )
+
+pred_outcome_pitch_adjusted <- adjust_outcome_for_approach(
+  pred_outcome_pitch = data_with_pred,
+  approach = approach_grid,
+  approach_model = approach_model
+)
+
+# Print a pitch-level summary of approach effects
+pred_outcome_pitch_adjusted |>
+  dplyr::group_by(bat_speed = strikes_bat_speed, swing_length = strikes_swing_length, strikes) |>
+  dplyr::summarize(
+    mean_prob_contact = weighted.mean(prob_contact, w = prob_swing),
+    mean_pred_hit = weighted.mean(pred_hit, w = prob_swing * prob_contact * prob_fair),
+    .groups = "drop"
+  ) |>
+  print(n = 30)
+
+# Calculate a plate-appearance level summary of approach effects
+approach_run_value <- pred_outcome_pitch_adjusted |>
+  dplyr::group_by(strikes_bat_speed, strikes_swing_length, balls, strikes) |>
+  dplyr::summarize(   # NOTE: This part should probably be functionized
+    mean_prob_hbp = mean((1 - prob_swing) * prob_hbp),
+    mean_prob_ball = mean((1 - prob_swing) * (1 - prob_hbp) * (1 - prob_strike)),
+    mean_prob_strike = mean((1 - prob_swing) * (1 - prob_hbp) * prob_strike + prob_swing * (1 - prob_contact)),
+    mean_prob_foul = mean(prob_swing * prob_contact * (1 - prob_fair)),
+    mean_prob_fair = mean(prob_swing * prob_contact * prob_fair),
+    mean_pred_hit = weighted.mean(pred_hit, w = prob_swing * prob_contact * prob_fair),
+    .groups = "drop"
+  ) |>
+  dplyr::group_by(strikes_bat_speed, strikes_swing_length) |>
+  calculate_pred_outcome_pa(linear_weight = linear_weight)
